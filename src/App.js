dae7367de65c56fba8ec28d130e1a31d90faa444
Abigail-Boatemaa-Owusu <47898685+Abigail-Boatemaa-Owusu@users.noreply.github.com>
2020-08-13 11:57:55 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react'
+import './App.css'
+
+import Main from './Component/Main'
+
+import Header  from './Component/Header'
+
+
+
+class App extends Component {
+render() { 
+  return ( 
+    <div>
+        <Header/>
+        <Main/>
+       
+       
+
+    </div>  
+   );
+
+}}
+
+
+export default App;
