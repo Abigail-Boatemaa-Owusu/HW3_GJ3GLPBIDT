d3b9e6d982d9e9bff3976de84d2fe026ee18a8f8
Abigail-Boatemaa-Owusu <47898685+Abigail-Boatemaa-Owusu@users.noreply.github.com>
2020-08-13 11:58:05 -0700

    Create Header.js

---
+++ src/Component/Header.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+
+
+
+class Header  extends Component {
+    render(){
+        return(    
+        <div>
+         
+    <nav className="navbar">
+      <div className="container">
+        <a href="hulk">Hulk</a>
+        
+        <a href="hulk" className="logo is-active">Mavel</a>
+        <a href="hulk">Venom</a>
+      </div>
+    </nav>
+         </div> 
+           
+        
+        )
+    }
+    
+}
+export default Header
